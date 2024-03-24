14c9ddaf74e52b31de495bac2b480c4b64fb64ce
Armin Schreger <armin@schreger.at>
2024-03-24 09:30:55 +0100

    Add reveal functionality

---
+++ public/javascripts/application.js
@@ -0,0 +1,7 @@
+
+function reveal() {
+  const element = document.getElementById("revealbutton");
+  element.remove();
+  const element2 = document.getElementById("bin-content");
+  element2.style.display = "block";
+}
