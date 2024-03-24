f714c360587ef8257135b2779a2c11cd58a413a9
Alexander Rusa <alex@rusa.at>
2024-03-24 15:01:22 +0100

    Add random_id to test

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -17,6 +17,7 @@ describe ApplicationController do
     post '/', bin: { payload: 'Hello, World!' }
     expect(last_response.status).to eq(200)
     expect(Bin.count).to eq(1)
+    expect(last_response.body).to include("/bins/#{Bin.last.random_id}")
   end
 
   it 'does not save a new bin without a payload' do
