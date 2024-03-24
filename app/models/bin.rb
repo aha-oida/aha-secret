c0930d07f2d323bbe1a6e0cee94d20671595116c
Alexander Rusa <alex@rusa.at>
2024-03-24 16:37:00 +0100

    Replace before_create with has_secure_token

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -1,8 +1,4 @@
 class Bin < ActiveRecord::Base
   validates :payload, presence: true
-  before_create :generate_random_id
-
-  def generate_random_id
-    self.random_id = SecureRandom.hex(30)
-  end
+  has_secure_token :random_id
 end
