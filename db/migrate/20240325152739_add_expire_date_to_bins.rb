30b8bbfd29f4e1bf216b0c80d1cb8f52c3075cea
Armin Schreger <armin@schreger.at>
2024-03-25 18:40:30 +0100

    Fix default expire_date with plain sql

--- db/migrate/20240325152739_add_expire_date_to_bins.rb
+++ db/migrate/20240325152739_add_expire_date_to_bins.rb
@@ -1,5 +1,5 @@
 class AddExpireDateToBins < ActiveRecord::Migration[7.1]
   def change
-    add_column :bins, :expire_date, :datetime, default: Time.now + 7.days
+    add_column :bins, :expire_date, :datetime, default: -> { "datetime('now','+7 day','localtime')" }
   end
 end
