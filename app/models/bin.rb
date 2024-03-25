3658acdabda2f2d2fd1bbf6079476bfc0e99c2aa
Armin Schreger <armin@schreger.at>
2024-03-25 17:02:40 +0100

    Extend Bin model with expire related methods, add more specs

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
-# Model for the Bin-Record
 class Bin < ActiveRecord::Base
   validates :payload, presence: true, length: { maximum: 10_000 }
   has_secure_token :random_id
+
+  scope :expired, -> { where('expire_date < ?', Time.now) }
+
+  def expired?
+    expire_date < Time.now
+  end
+
+  def self.cleanup
+    expired.destroy_all
+  end
 end
--- spec/bin_model_spec.rb
+++ spec/bin_model_spec.rb
@@ -13,4 +13,32 @@ describe Bin do
     bin = Bin.new(payload: '')
     expect(bin.save).to be false
   end
+
+  it 'has a expire_date' do
+    bin = Bin.new(payload: 'Hello, World!')
+    expect(bin.save).to be true
+    expect(bin.expire_date).not_to be nil
+  end
+
+  it 'has a expired? method' do
+    bin = Bin.create(payload: 'Hello, World!')
+    expect(bin.expired?).to be false
+    bin.update(expire_date: Time.now - 1.day)
+    expect(bin.expired?).to be true
+  end
+
+  it 'can be filtered by expiration' do
+    bin = Bin.create(payload: 'Hello, World!')
+    expect(Bin.expired).to eq []
+    bin.update(expire_date: Time.now - 1.day)
+    expect(Bin.expired).to eq [bin]
+  end
+
+  it 'has a cleanup method' do
+    bin = Bin.create(payload: 'Hello, World!')
+    bin.update(expire_date: Time.now - 1.day)
+    expect(Bin.expired).to eq [bin]
+    Bin.cleanup
+    expect(Bin.expired).to eq []
+  end
 end
