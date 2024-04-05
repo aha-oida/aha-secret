6676d047be8ecf41634d46e6f7ed5b38b0125896
Alexander Rusa <alex@rusa.at>
2024-04-05 13:59:12 +0200

    Add and fix new cops

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-ENV['RACK_ENV'] ||= 'development'
+environment = ENV['RACK_ENV'] || 'development'
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+Bundler.require(:default, environment)
 require 'yaml'
 # require 'uri'
 require 'logger'
 
 DB_CONFIG = YAML.load_file('./config/database.yml')
-puts "Starting with Environment: #{ENV['RACK_ENV']}"
-ActiveRecord::Base.establish_connection(DB_CONFIG[ENV['RACK_ENV']])
+puts "Starting with Environment: #{environment}"
+ActiveRecord::Base.establish_connection(DB_CONFIG[environment])
 # require './app/controllers/application_controller'
 require_all 'app'
