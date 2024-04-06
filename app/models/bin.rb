8b33f1f055f975a8e473c02385871c8f38104a3a
Armin Schreger <armin@schreger.at>
2024-04-06 07:10:06 +0200

    Compare with (date_)time instead of a date only

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -14,7 +14,7 @@ class Bin < ActiveRecord::Base
   def expire_date_cannot_be_bigger_than_7_days
     return unless expire_date
 
-    errors.add(:expire_date, "Can't be bigger than 7 days") if expire_date > (Date.today + 7.days)
+    errors.add(:expire_date, "Can't be bigger than 7 days") if expire_date > (Time.now + 7.days)
   end
 
   def expired?
