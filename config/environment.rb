65296af1dae164159bf1e6b881484e544d34dab0
Armin Schreger <armin@schreger.at>
2024-04-12 16:51:20 +0200

    Trim environment, use `sinatra/activerecord` instead of `activerecord` directly

--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
-environment = ENV['RACK_ENV'] || 'development'
-
 require 'bundler/setup'
-# Bundler.require(:default, environment)
-require 'active_record'
 require 'require_all'
 require 'sinatra/base'
 require 'sinatra/config_file'
+require 'sinatra/activerecord'
 require 'yaml'
-# require 'uri'
 require 'logger'
-
-DB_CONFIG = YAML.load_file('./config/database.yml')
-puts "Starting with Environment: #{environment}"
-ActiveRecord::Base.establish_connection(DB_CONFIG[environment])
-# require './app/controllers/application_controller'
 require_all 'app'
