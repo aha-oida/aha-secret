d22e132711e8f39a62a8e610b10706088e41ac96
Alexander Rusa <alex@rusa.at>
2024-09-22 21:57:42 +0200

    Simplify sanitizing params

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,26 +5,4 @@ module Helpers
   def bin_retrieval_url(bin)
     "#{request.base_url}/bins/#{bin.id}"
   end
-
-  # strip params to only: payload (text), has_password (boolean) and retention (integer)
-  def reduce_params(params)
-    # check bin's params
-    params[:bin] = reduce_bin_params(params[:bin]) if params[:bin]
-
-    # check retention - which is given in seconds and converted to DateTime later
-    params.delete(:retention) unless params[:retention]&.to_i&.positive?
-    # check top level keys
-    params.each_key do |key|
-      params.delete(key) unless %w[bin retention].include?(key.to_s)
-    end
-    params
-  end
-
-  def reduce_bin_params(params)
-    # check bin's params
-    params.each_key do |key|
-      params.delete(key) unless %w[payload has_password].include?(key.to_s)
-    end
-    params
-  end
 end
