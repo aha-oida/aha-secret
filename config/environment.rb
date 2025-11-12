f634e08521f54f51fd98728c02eee22648feded7
Armin Schreger <armin@schreger.at>
2025-11-12 22:33:46 +0100

    Refactor database connection and migration checking setup

--- config/environment.rb
+++ config/environment.rb
@@ -12,9 +12,11 @@ require 'yaml'
 require 'logger'
 require 'i18n'
 require 'i18n/backend/fallbacks'
+require_relative 'initializers/migration_check'
 
 # Database connection setup for Sequel
-DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://db/development.sqlite3')
+require_relative 'initializers/database'
+
 DB.loggers << Logger.new($stdout) if ENV['RACK_ENV'] == 'development'
 
 # Determine if we're running in a context where we should skip migration checks and model loading
@@ -23,23 +25,7 @@ running_db_task = running_rake && Rake.application.top_level_tasks.any? { |task|
 running_tests = ENV['RACK_ENV'] == 'test'
 
 # Check for pending migrations before loading models
-require 'sequel/extensions/migration'
-if !running_tests && !running_rake
-  begin
-    Sequel::Migrator.check_current(DB, 'db/migrate')
-  rescue Sequel::Migrator::NotCurrentError
-    warn "\n" + ('=' * 80)
-    warn 'ERROR: Database migrations are pending!'
-    warn '=' * 80
-    warn 'Your database schema is not up to date.'
-    warn 'Please run the following command to apply pending migrations:'
-    warn ''
-    warn '  bundle exec rake db:migrate'
-    warn ''
-    warn '=' * 80 + "\n"
-    exit 1
-  end
-end
+check_pending_migrations! unless running_tests || running_rake
 
 # Load application code unless running db rake tasks (which may run before tables exist)
 unless running_db_task
---
+++ config/initializers/migration_check.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative 'database'
+require 'sequel/extensions/migration'
+
+def warn_pending_migrations
+  warn "\n#{'=' * 80}"
+  warn 'ERROR: Database migrations are pending!'
+  warn '=' * 80
+  warn 'Your database schema is not up to date.'
+  warn 'Please run the following command to apply pending migrations:'
+  warn ''
+  warn '  bundle exec rake db:migrate'
+  warn ''
+  warn "#{'=' * 80}\n"
+  exit 1
+end
+
+def ensure_schema_migrations_filename_column!
+  columns = DB.schema(:schema_migrations).map(&:first)
+  return if columns.include?(:filename)
+
+  DB.alter_table(:schema_migrations) { add_column :filename, String }
+end
+
+def populate_schema_migrations_filenames!
+  migrations_dir = File.expand_path('../../db/migrate', __dir__)
+  DB[:schema_migrations].where(filename: nil).each do |row|
+    version = row[:version].to_s
+    file = Dir.glob("#{migrations_dir}/#{version}_*.rb").first
+    if file
+      filename = File.basename(file)
+      DB[:schema_migrations].where(version: row[:version]).update(filename: filename)
+    end
+  end
+end
+
+def check_pending_migrations!
+  ensure_schema_migrations_filename_column!
+  populate_schema_migrations_filenames!
+  Sequel::TimestampMigrator.check_current(DB, 'db/migrate')
+rescue Sequel::Migrator::NotCurrentError
+  warn_pending_migrations
+end
+
+# Only check for pending migrations unless running tests or rake tasks
+unless (defined?(running_tests) && running_tests) || (defined?(running_rake) && running_rake)
+  check_pending_migrations!
+end
