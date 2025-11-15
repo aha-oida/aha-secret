661e1d08fd4d949c6b417c14190d9323f4a7991b
Armin Schreger <armin@schreger.at>
2025-11-15 11:51:54 +0100

    Remove redundant set_column_default from bins migration

--- db/migrate/20240325152739_add_expire_date_to_bins.rb
+++ db/migrate/20240325152739_add_expire_date_to_bins.rb
@@ -1,6 +1,5 @@
 Sequel.migration do
   change do
-    set_column_default :bins, :expire_date, Sequel.lit("datetime('now','+7 day','localtime')")
     add_column :bins, :expire_date, DateTime, default: Sequel.lit("datetime('now','+7 day','localtime')")
   end
 end
