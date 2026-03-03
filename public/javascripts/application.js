01a289bdd330f6949725e4175b0cdfca9f6fbb2a
Armin Schreger <armin@schreger.at>
2026-03-03 16:43:45 +0100

    Fix indendation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -99,13 +99,12 @@ function addPassword() {
   additionalPasswordField.style.opacity = haspw.checked ? "1" : "0";
 
   if (haspw.checked) {
-      if (createSecretButton && (!addPasswordField || addPasswordField.value.length === 0)) {
-        createSecretButton.setAttribute("disabled", "disabled");
-      }
+    if (createSecretButton && (!addPasswordField || addPasswordField.value.length === 0)) {
+      createSecretButton.setAttribute("disabled", "disabled");
     }
-  else if (createSecretButton) {
-        createSecretButton.removeAttribute("disabled");
-      }
+  } else if (createSecretButton) {
+    createSecretButton.removeAttribute("disabled");
+  }
 }
 
 function showRandomSettings() {
